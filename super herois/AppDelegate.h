066e417d5669620528d84738b909e74e2c1c5e83
Paulo Sales <paulovitorns@gmail.com>
2017-05-29 12:40:24 -0300

    criação do projeto no xcode

---
+++ super herois/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  super herois
+//
+//  Created by Paulo Sales on 29/05/17.
+//  Copyright © 2017 Paulo Sales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ super herois/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  super herois
+//
+//  Created by Paulo Sales on 29/05/17.
+//  Copyright © 2017 Paulo Sales. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
